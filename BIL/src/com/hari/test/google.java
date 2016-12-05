ad08e93a0869d6f3f2b75c0b6e0e3a99e62d0e6e
Hari <hari.salamaddur@gmail.com>
2016-12-05 12:30:22 +0530

    Build path changed

--- BIL/src/com/hari/test/google.java
+++ BIL/src/com/hari/test/google.java
@@ -4,8 +4,6 @@ public class google {
 
 	public static void main(String[] args) {
 		
-		System.out.println("GIT One");
-		System.out.println("GIT Two");
-		System.out.println("GIT Three");
+		
 	}
 }
